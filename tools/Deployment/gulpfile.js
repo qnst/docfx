29b372fdf58cc365f96d7ef8bd735d0dbff621e2
Sharanya <v-shsh11@microsoft.com>
2020-05-12 13:43:59 +0800

    inline code sign

--- tools/Deployment/gulpfile.js
+++ tools/Deployment/gulpfile.js
@@ -251,10 +251,9 @@ gulp.task("syncBranchCore", () => {
 gulp.task("test", gulp.series("clean", "build", "e2eTest", "pack", "publish:myget-test"));
 gulp.task("dev", gulp.series("clean", "build", "e2eTest"));
 gulp.task("dev:build", gulp.series("clean", "build", "e2eTest"));
-gulp.task("dev:release", gulp.series("pack", "publish:myget-dev", "publish:azdevops-perf-login", "publish:azdevops-perf", "publish:azdevops-internal-login", "publish:azdevops-internal", "publish:azdevops-ppe-login", "publish:azdevops-ppe"));
+gulp.task("dev:release", gulp.series("pack"));
 
-gulp.task("master:build", gulp.series("clean", "build:release", "e2eTest", "updateGhPage"));
+gulp.task("master:build", gulp.series("clean", "build:release", "e2eTest"));
 gulp.task("master:pack", gulp.series("pack"));
-gulp.task("master:release", gulp.series("packAssetZip", "publish:myget-master", "publish:azdevops-prod-login", "publish:azdevops-prod", "publish:gh-release", "publish:gh-asset", "publish:chocolatey"));
 
 gulp.task("default", gulp.series("dev"));
